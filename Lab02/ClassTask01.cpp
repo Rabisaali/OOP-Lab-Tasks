b8828b4244b506ae652c88c2a0c5559b6e5099de
Rabisa <rabisa55511@gmail.com>
2026-02-05 15:03:15 +0500

    adding class task 1

--- Lab02/ClassTask01.cpp
+++ Lab02/ClassTask01.cpp
@@ -1,6 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define LL long long int
 int main () {
-
+    int a=15;
+    int* ptr = &a;
+    cout << "The value of variable is: " << a << endl;
+    cout << "The address of variable through the ampersand operator is: " << &a << endl;
+    cout << "The address of variable through pointer is: " << ptr << endl;
+    cout << "The value of variable through pointer is: " << *ptr << endl;
+    return 0;
 }
