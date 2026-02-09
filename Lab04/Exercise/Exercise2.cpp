7b4c325cd15f56031bc6bc1bf02f69f948f1ead9
Rabisa <rabisa55511@gmail.com>
2026-02-09 09:52:01 +0500

    adding lab 4 tasks

--- Lab04/Exercise/Exercise2.cpp
+++ Lab04/Exercise/Exercise2.cpp
@@ -5,4 +5,30 @@ using namespace std;
 class Book {
     public:
         string title;
+
+        Book (string t) {
+            title = t;
+            cout << "Parameterized COnstructor Called" << endl;
+        }
+
+        Book (const Book &b) {
+            title = b.title;
+            cout << "Copy Constructor Called" << endl;
+        }
+
+        void display() {
+            cout << "Book Title: " << title << endl;
+        }
+};
+
+int main () {
+    Book b1 ("Object Oriented Programming");
+    b1.display();
+
+    cout << endl;
+
+    Book b2 = b1;
+    b2.display();
+
+    return 0;
 }
