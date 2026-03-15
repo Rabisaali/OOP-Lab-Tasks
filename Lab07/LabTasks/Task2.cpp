0dbbd5050724163e100c7d1e31a22b2a97fa6c7a
Rabisa <rabisa55511@gmail.com>
2026-03-15 06:42:23 +0500

    adding task 2

---
+++ Lab07/LabTasks/Task2.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class Employee {
+    public:
+        void calculateSalary () {
+            cout << "The salary of the employee is: Rs. 55000" << endl; 
+        }
+};
+
+class Manager: public Employee {
+    public:
+        void calculateSalary () {
+            cout << "The salary of the manager is: Rs. 88000" << endl;
+        }
+};
+
+class Developer: public Employee {
+    public:
+        void calculateSalary () {
+            cout << "The salary of the developer is: Rs. 85000" << endl;
+        }
+};
+
+class Intern: public Employee {
+    public:
+        void calculateSalary () {
+            cout << "The salary of the intern is: Rs. 35000" << endl;
+        }
+};
+
+int main () {
+    Manager m1;
+    m1.calculateSalary();
+    Developer d1;
+    d1.calculateSalary();
+    Intern i1;
+    i1.calculateSalary();
+}
