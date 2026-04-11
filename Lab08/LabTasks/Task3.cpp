548e06668871e5d2b355205bd13738df8b0ddbd2
Rabisa <rabisa55511@gmail.com>
2026-04-11 23:43:42 +0500

    adding task 3 of labtasks

---
+++ Lab08/LabTasks/Task3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+    private:
+        string ID;
+        string name;
+    public:
+        Person(string id, string n) : ID(id), name(n) {}
+        void displayPerson() {
+            cout << "ID: " << ID << endl;
+            cout << "Name: " << name << endl;
+        } 
+};
+
+class Student : virtual public Person {
+    private:
+      float GPA;
+    public:
+        Student(float g, string id, string name) : Person(id, name) {
+            GPA = g;
+        }
+        void displayStudent () {
+            cout << "GPA: " << GPA << endl;
+        }
+};
+class Employee : virtual public Person {
+    private:
+        int salary;
+    public:
+        Employee(int s, string id, string name) : Person(id, name) {
+            salary = s;
+        }
+        void displayEmployee() {
+            cout << "Salary: " << salary << endl;
+        }
+};
+class TeachingAssistant : public Student, public Employee {
+    public:
+        TeachingAssistant(int s, float g, string id, string n) : Student (g, id, n) , Employee (s, id, n), Person(id, n) {}
+        void displayTA() {
+            displayPerson();
+            displayStudent();
+            displayEmployee();
+        }
+};
+
+int main () {
+    TeachingAssistant ta(25000, 3.4, "25K-0876", "Hamza");
+    ta.displayTA();
+}
