e80a53e64f853a155c6c569b9ed9a048a6345919
Rabisa <rabisa55511@gmail.com>
2026-04-22 23:44:57 +0500

    adding exercise 2

--- Lab10/Exercises/Exercise2.cpp
+++ Lab10/Exercises/Exercise2.cpp
@@ -25,6 +25,10 @@ class Calculator {
         }
 
         T division () {
+            if (num2 == 0) {
+                cout << "Division by zero not allowed!" << endl;
+                return 0;
+            }
             return (num1/num2);
         }
 };
