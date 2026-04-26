86b531053d5cdcc089d521add3718803e4c4123f
Rabisa <rabisa55511@gmail.com>
2026-04-26 11:34:57 +0500

    adding task 3

---
+++ Lab11/Tasks/Task3.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+class Student {
+    private:
+        int id;
+        string name;
+        float gpa;
+    public:
+        void input () {
+            cout << "Enter ID: ";
+            cin >> id;
+            cin.ignore();
+            cout << "Enter Name: ";
+            getline(cin, name);
+            cout << "Enter GPA: ";
+            cin >> gpa;
+        }
+
+        void writeToFile() {
+            fstream file;
+            file.open("Task3.txt", ios::app);
+            file << "ID: " << id << " | Name: " << name << " | GPA: " << gpa << endl;
+            file.close();
+        }
+
+        void displayFromProgramMemory() {
+            cout << "ID: " << id << endl;
+            cout << "Name: " << name << endl;
+            cout << "GPA: " << gpa << endl;
+        }
+
+        void displayFromFile() {
+            fstream file;
+            file.open("Task3.txt", ios::in);
+            if (!file) {
+                cout << "Could not open file for reading." << endl;
+                return;
+            }
+            cout << "Reading from file: " << endl;
+            string line;
+            while(getline(file, line)) {
+                cout << line << endl;
+            }
+            file.close();
+        }
+
+};
+
+int main () {
+    Student s[5];
+    for (int i=0; i<5; i++) {
+        cout << "Adding records for student " << i+1 << endl;
+        s[i].input();
+        s[i].writeToFile();
+    }
+    cout << "Student record saved!" << endl;
+
+    s[0].displayFromFile();
+    
+    return 0;
+}
