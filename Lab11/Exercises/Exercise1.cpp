95dbc2ac916bb5c14a9046c8cb0901d86de72f04
Rabisa <rabisa55511@gmail.com>
2026-04-26 10:23:23 +0500

    adding task 1

---
+++ Lab11/Exercises/Exercise1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main () {
+    int age;
+    string name;
+
+    cout << "Enter your name: ";
+    getline(cin, name);
+    cout << "Enter your age: ";
+    cin >> age;
+
+    fstream file;
+    file.open("Exercise1.txt", ios::out);
+
+    if (!file) {
+        cout << "Error: file could not be opened!" << endl;
+        return 0;
+    }
+
+    file << "Name: " << name <<  endl;
+    file << "Age: " << age << endl;
+
+    file.close();
+    cout << "Data successfully written to file." << endl;
+}
